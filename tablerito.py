103b5ef1bac8cf1d0c90850bc8f0c27668b1d6c2
juanpago11 <juanpago11@gmail.com>
2026-04-16 09:53:26 -0500

    Update tablerito.py

--- tablerito.py
+++ tablerito.py
@@ -1,1 +1,33 @@
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
 
+st.title("Tablero para dibujo")
+
+with st.sidebar:
+    st. subheader ("Propiedades del Tablero")
+# Canvas dimensions (moved to the top)
+    st. subheader ("Dimensiones del Tablero")
+    canvas_width = st.slider("Ancho del tablero", 300, 700, 500, 50)
+    canvas _height = st.slider("Alto del tablero", 200,600,300,50)
+
+    drawing_mode = st.selectbox(
+        "Herramienta de Dibujo:",
+        ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
+    )
+  
+    stroke_width • st.slider("Selecciona el ancho de línea", 1, 30, 15 )
+                             
+    stroke_color • st.color_picker("Color de trazo", "#FFFFFF")
+# Background color
+    bg_color = st.color picker("Color de fondo", "#000000")
+# Create a canvas component with dynamic key
+canvas_result = st_canvas(
+    fil1_color="rgba(255, 165, 0, 0.3)",
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    height=canvas_height,
+    width=canvas_width,
+    drawing mode=drawing_mode,
+    key=f"canvas_{canvas_width}_{canvas_height}",
+    )
